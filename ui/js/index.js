a481a8d75920cbbf500176689ecac7b2f0b89090
avapul <vabimana@gmail.com>
2019-01-25 16:47:57 +0200

    Initialize repo and finish home page UI

---
+++ ui/js/index.js
@@ -0,0 +1,39 @@
+class Dropdown {
+    constructor(ui) {
+        this.active = false;
+        this.elts = { ...ui
+        };
+        this.addListeners();
+    }
+
+    show() {
+        this.elts.container.classList.add('show');
+        this.elts.overlay.classList.add('show');
+        this.active = true;
+
+    }
+
+    hide() {
+        this.elts.container.classList.remove('show');
+        this.elts.overlay.classList.remove('show');
+        this.active = false;
+    }
+    addListeners() {
+        this.elts.trigger.addEventListener('click', () => {
+            this.show()
+        });
+        this.elts.overlay.addEventListener('click', () => {
+            if (this.active) {
+
+                this.hide()
+            }
+        });
+    }
+}
+
+let ui = {
+    trigger: document.getElementsByClassName('dropdown-trigger')[0],
+    container: document.getElementsByClassName('dropdown')[0],
+    overlay: document.getElementsByClassName('overlay-sc')[0],
+}
+let dropdown = new Dropdown(ui);
