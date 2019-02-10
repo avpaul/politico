801d1c3a539d0d41302ae96261a6546a7e5aafd8
avapul <vabimana@gmail.com>
2019-02-10 18:42:40 +0200

    Finish politicians page redisign

---
+++ public/javascripts/index.js
@@ -0,0 +1,41 @@
+class Dropdown {
+    constructor(ui) {
+        this.active = false;
+        this.elts = { ...ui };
+    }
+
+    init() {
+        this.addListeners();
+    }
+
+    show() {
+        this.elts.container.classList.add('show');
+        this.elts.overlay.classList.add('show');
+        this.active = true;
+    }
+
+    hide() {
+        this.elts.container.classList.remove('show');
+        this.elts.overlay.classList.remove('show');
+        this.active = false;
+    }
+
+    addListeners() {
+        this.elts.trigger.addEventListener('click', () => {
+            this.show();
+        });
+        this.elts.overlay.addEventListener('click', () => {
+            if (this.active) {
+                this.hide();
+            }
+        });
+    }
+}
+
+const ui = {
+    trigger: document.getElementsByClassName('dropdown-trigger')[0],
+    container: document.getElementsByClassName('dropdown')[0],
+    overlay: document.getElementsByClassName('overlay-sc')[0],
+};
+const Drop = new Dropdown(ui);
+Drop.init();
