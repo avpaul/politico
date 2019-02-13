398991ab3bf4ae1aefb58864021a15de6d4e6dd7
avapul <vabimana@gmail.com>
2019-02-13 19:03:10 +0200

    Fix failing test

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const logger = require('morgan');
 
 const apiRouter = require('./server/routes/api');
 const indexRouter = require('./server/routes/index');
 
 const app = express();
 
+app.use(logger('dev'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/v1', apiRouter);
-
-// catch 404
-app.use((req, res) => {
-    res.json();
-});
 
 module.exports = app;
